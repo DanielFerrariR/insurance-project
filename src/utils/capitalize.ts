5ec1c0c8b46c4801c6679979ab3b595b15fa8d42
Daniel Ferrari <danielferrari@live.com>
2021-08-07 23:47:15 -0300

    refactor: adding capitalize function tsdoc description

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,4 +1,9 @@
+/**
+ * Capitalize a string
+ * @param word - The string
+ * @returns The capitalized string
+ */
 const capitalize = (word: string): string =>
-  word.charAt(0).toUpperCase() + word.slice(1)
+  word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 
 export default capitalize
